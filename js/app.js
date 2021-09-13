9b7d0da8bad10547769cfc89b4819c44eae1db9a
Aal Emraan <developer.aalemraan@gmail.com>
2021-09-13 12:56:30 +0600

    url updated

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 const loadProducts = () => {
-  const url = "data.json";
-  // const url = `https://fakestoreapi.com/products`;
+  const url = `https://fakestoreapi.com/products`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => showProducts(data));
